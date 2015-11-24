3ba1e49399c28b9bf745d7d259cff9bc73eebdc6
Louis Pipkin <louis@louispipkin.com>
2015-11-24 13:48:42 -0500

    Initial Commit

---
+++ Eratosthenes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Eratosthenes
+//
+//  Created by Louis Pipkin on 11/24/15.
+//  Copyright © 2015 Louis Pipkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
